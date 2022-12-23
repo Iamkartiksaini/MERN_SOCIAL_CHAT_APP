32a9988b648d0655e93c6c432456d78da099de2a
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2022-12-23 15:44:19 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const mongoose = require(`mongoose`);
-
-// const cors = require("cors");
-// app.use(cors());
-// mongoose.set("strictQuery", false);
-// // const localUrl = "mongodb://0.0.0.0:27017/accounts";
-// const localUrl2 = "mongodb://0.0.0.0:27017/codedamn";
-// const chat_hub_local = "mongodb://0.0.0.0:27017/chat_hub";
-// const cloudUrl =
-//   "mongodb+srv://kartik:HarshSaini@learning.pm3vwfv.mongodb.net/usersList";
-
-// mongoose.connect(localUrl2, {
-//   useNewUrlParser: true,
-// });
-
-// const db = mongoose.connection;
-// db.on("error", (error) => console.error(error));
-// db.once("open", () => console.log("connected to database"));
-
-// app.use(express.json());
-
-// const users = require("./Router/router");
-
-// app.use("/users", users);
-
-// app.listen(4000, () => {
-//   console.log("server started");
-// });
 
 const express = require("express");
 const mongoose = require("mongoose");
