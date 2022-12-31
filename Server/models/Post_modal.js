116bb7cedbcb7a5ee35ab7ff916105c5babc1b0d
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2022-12-31 11:49:43 +0530

    Post modal added

---
+++ Server/models/Post_modal.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const twitterPostSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  userID: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const TwitterPost = mongoose.model("Posts", twitterPostSchema);
+
+module.exports = TwitterPost;
